e121b0bdfa169f96b2d8e1281d82ea36dde44d85
Eva <1cobo.eva@gmail.com>
2020-02-05 21:02:44 +0100

    Updates

---
+++ precache-manifest.496789c64a9b0a1d1655535f171b0ce6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034445f4aa734ea97272eed1e8a2901b",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "d53d6bb6acb5a75e9df6",
+    "url": "/repository-name/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bd73abb7266e4604c074",
+    "url": "/repository-name/static/css/main.8c7e0b56.chunk.css"
+  },
+  {
+    "revision": "d53d6bb6acb5a75e9df6",
+    "url": "/repository-name/static/js/2.080999ff.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/repository-name/static/js/2.080999ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd73abb7266e4604c074",
+    "url": "/repository-name/static/js/main.c62c1e36.chunk.js"
+  },
+  {
+    "revision": "737302d2f78450c31c6e",
+    "url": "/repository-name/static/js/runtime-main.036ddd69.js"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/repository-name/static/media/GameOfThrones.3af178ec.ttf"
+  },
+  {
+    "revision": "0bc83c2b8d314ceea1bbe186de0341b5",
+    "url": "/repository-name/static/media/casa.0bc83c2b.svg"
+  },
+  {
+    "revision": "7ebf4b2a78ace0eb584db80b18927758",
+    "url": "/repository-name/static/media/cat.7ebf4b2a.jpg"
+  },
+  {
+    "revision": "46457879840f6939d02c6e0f0f859598",
+    "url": "/repository-name/static/media/image1.46457879.png"
+  },
+  {
+    "revision": "5dac8ace5213613ef4f27998a2fac899",
+    "url": "/repository-name/static/media/spain1.5dac8ace.svg"
+  },
+  {
+    "revision": "9ca69872b559f7e0210583cb884f80a7",
+    "url": "/repository-name/static/media/uk1.9ca69872.svg"
+  }
+]);
