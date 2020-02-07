3486d6b1fc6a2fd30dc15ea71bcdd06c1bc5c66b
Eva <1cobo.eva@gmail.com>
2020-02-07 10:33:00 +0100

    nombre

--- src/pages/characters/ItemDetail.js
+++ src/pages/characters/ItemDetail.js
@@ -26,46 +26,48 @@ function ItemDetail({ match }) {
         <div className="b-container__all">
             <div className="container-fluid">
                 <div className="itemDetail" className="row">
-                    <div>
+                    <div className="image">
                         <img src={item.image} alt=" " key={item.image}></img>
                         <h4 className="font-characters">{item.name}</h4>
                     </div>
                 </div>
-<div className="container" className="prueba">
-                <div className="row">
-                    <div col="col-2">
+            </div>
+
+            <div className="container" className="prueba">
+                <ul className="row" className="b.row">
+                    <li col="col-2">
                         <h4 className="font-characters">ALIANZAS</h4>
                         <p className="lato">{item.allegiances}</p>
-                    </div>
+                    </li>
 
-                    <div col="col-2">
+                    <li col="col-2">
                         <h4 className="font-characters">APARICIONES</h4>
                         <p className="lato">{item.appearances}</p>
-                    </div>
+                    </li>
 
-                    <div col="col-2">
+                    <li col="col-2">
                         <h4 className="font-characters">PADRE</h4>
                         <p className="lato">
                             {item.father}
                         </p>
-                    </div>
+                    </li>
 
-                    <div col="col-2">
+                    <li col="col-2">
                         <h4 className="font-characters">DESCENDIENTES</h4>
                         <p className="lato">
                             {item.siblings}
                         </p>
-                    </div>
+                    </li>
 
-                    <div col="col-2">
+                    <li col="col-2">
                         <h4 className="font-characters">TITULOS</h4>
                         <p className="lato">
                             {item.titles}
                         </p>
-                    </div>
-                    </div>
-                </div>
-        </div>
+                    </li>
+                </ul>
+
+            </div>
         </div>
     );
 }
